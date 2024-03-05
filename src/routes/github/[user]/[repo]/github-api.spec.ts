3cd1711e14a44979e92689fdb127cb57f0362b59
Edis Neziric <e.neziric@3dqr.de>
2024-03-05 21:17:24 +0100

    Inital

--- src/routes/github/[user]/[repo]/github-api.spec.ts
+++ src/routes/github/[user]/[repo]/github-api.spec.ts
@@ -1,8 +1,13 @@
-import { describe, it } from "vitest";
+import { describe, it } from 'vitest';
+import { GithubApi } from '~/routes/github/[user]/[repo]/github-api';
 
-describe("github-api", () => {
-  describe("getRepository", () => {
-    it.todo("should return repository information");
-    it.todo("should timeout after x seconds with time out response");
+describe('github-api', () => {
+  describe('getRepository', () => {
+    it.todo('should return repository information', async ({ expect }) => {
+      const api = new GithubApi(undefined);
+      const response = await api.getRepository('mhevery', 'qwik');
+      expect(response).toMatchSnapshot();
+    });
+    it.todo('should timeout after x seconds with time out response');
   });
 });
